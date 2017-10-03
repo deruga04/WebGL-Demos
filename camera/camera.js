5f8aab7f6a69717c44e15667029acec2656cfa26
Derek Li <deruga@Dereks-MacBook-Air.local>
2017-10-03 13:30:43 -0400

    camera added

---
+++ camera/camera.js
@@ -0,0 +1,43 @@
+var cameraLookFrom = vec3(5, 3, 8);
+var cameraLookAt = vec3(0, 0, 0);
+var cameraLookUp = vec3(0, 1, 0);
+var fovY = 50;
+var near = .1;
+var far = 100;
+
+
+
+var cameraVerticies;
+var cameraBufferID;
+
+
+function mouseButtonDown(x, y)
+{
+	// your code goes here
+}
+
+function mouseButtonUp(x,y)
+{
+	// your code goes here
+}
+
+function mouseMove(x,y)
+{
+	// your code goes here
+	document.getElementById("ScreenInfo").innerHTML = "(" + x + ", " +y + ")";
+}
+
+function initCamera()
+{
+	// your code goes here
+}
+
+function computeCameraOrientation()
+{
+	// your code goes here
+}
+
+function drawCameraControls()
+{
+	// your code goes here
+}
---
+++ camera/model.js
@@ -0,0 +1,126 @@
+var modelBufferID;
+var modelBuffer;
+
+var red = vec4(1,0,0,1);
+var green = vec4(0,1,0,1);
+var blue = vec4(0,0,1,1);
+var yellow = vec4(1,1,0,1);
+
+var ModelViewStack = [];
+
+function pushModelView()
+{
+	ModelViewStack.push(modelViewMatrix);
+}
+
+function popModelView()
+{
+	modelViewMatrix = ModelViewStack.pop();
+}
+
+function sendModelView()
+{
+	gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+}
+
+
+function initScene()
+{
+	modelBufferID = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, modelBufferID);
+	gl.bufferData(gl.ARRAY_BUFFER, 16*64, gl.STATIC_DRAW);	
+}
+
+function drawScene()
+{
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, modelBufferID);
+	gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(vPosition);
+	
+	gl.uniform4fv(objectColor, green);
+	drawGround();
+	
+	pushModelView();
+	modelViewMatrix = mult(modelViewMatrix, scalem(0.5, 0.5, 0.5));
+	modelViewMatrix = mult(modelViewMatrix, translate(0, 1, 0));
+	sendModelView();
+	gl.uniform4fv(objectColor, yellow);
+	drawCube();
+	
+	pushModelView();
+	modelViewMatrix = mult(modelViewMatrix, translate(4, 0, 0));
+	sendModelView();
+	gl.uniform4fv(objectColor, red);
+	drawCube();
+	popModelView();
+	
+	pushModelView();
+	modelViewMatrix = mult(modelViewMatrix, translate(-4, 0, 0));
+	sendModelView();
+	gl.uniform4fv(objectColor, blue);
+	drawCube();
+	popModelView();
+	
+	popModelView();
+}
+
+function drawGround()
+{
+	modelBuffer = [];
+	modelBuffer.push(vec4(-5, 0, -5));
+	modelBuffer.push(vec4( 5, 0, -5));
+	modelBuffer.push(vec4( 5, 0,  5));
+	modelBuffer.push(vec4(-5, 0,  5));
+	gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(modelBuffer));
+	gl.drawArrays(gl.LINE_LOOP, 0, modelBuffer.length);
+}
+
+function drawCube()
+{
+	var verticies = [vec4(-1, -1, -1), vec4(-1, -1, 1), vec4(-1, 1, -1), vec4(-1, 1, 1),
+					vec4(1, -1, -1), vec4(1, -1, 1), vec4(1, 1, -1), vec4(1, 1, 1)]; 
+					
+	modelBuffer = [];
+	modelBuffer.push(verticies[0]);
+	modelBuffer.push(verticies[1]);
+	modelBuffer.push(verticies[3]);
+	modelBuffer.push(verticies[2]);
+	gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(modelBuffer));
+	gl.drawArrays(gl.LINE_LOOP, 0, modelBuffer.length);
+	modelBuffer = [];
+	modelBuffer.push(verticies[5]);
+	modelBuffer.push(verticies[4]);
+	modelBuffer.push(verticies[6]);
+	modelBuffer.push(verticies[7]);
+	gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(modelBuffer));
+	gl.drawArrays(gl.LINE_LOOP, 0, modelBuffer.length);
+	modelBuffer = [];
+	modelBuffer.push(verticies[1]);
+	modelBuffer.push(verticies[5]);
+	modelBuffer.push(verticies[7]);
+	modelBuffer.push(verticies[3]);
+	gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(modelBuffer));
+	gl.drawArrays(gl.LINE_LOOP, 0, modelBuffer.length);
+	modelBuffer = [];
+	modelBuffer.push(verticies[4]);
+	modelBuffer.push(verticies[0]);
+	modelBuffer.push(verticies[2]);
+	modelBuffer.push(verticies[6]);
+	gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(modelBuffer));
+	gl.drawArrays(gl.LINE_LOOP, 0, modelBuffer.length);
+	modelBuffer = [];
+	modelBuffer.push(verticies[2]);
+	modelBuffer.push(verticies[3]);
+	modelBuffer.push(verticies[7]);
+	modelBuffer.push(verticies[6]);
+	gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(modelBuffer));
+	gl.drawArrays(gl.LINE_LOOP, 0, modelBuffer.length);
+	modelBuffer = [];
+	modelBuffer.push(verticies[1]);
+	modelBuffer.push(verticies[0]);
+	modelBuffer.push(verticies[4]);
+	modelBuffer.push(verticies[5]);
+	gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(modelBuffer));
+	gl.drawArrays(gl.LINE_LOOP, 0, modelBuffer.length);
+}
